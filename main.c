1354bf2d0613f35dd2d5f4f97ba3639a1915497d
solokhovir <sir1504@mail.ru>
2021-09-15 18:12:44 +0300

    New

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 int main(int argc, char** argv)
 {
 	char first[255], last[255];
+
 	askname(first, last);
 	printf("Hello, %s %s!\n", first, last);
+
+
+	printf("Enter your first name: ");
+	fgets(first, 255, stdin);
+	first[strlen(first) - 1] = '\0';
+
+	printf("Now enter your last name: ");
+	gets(last);
+
+	printf("Hello %s %s!\n", first, last);
+>>>>>>> f6fbd9b (Prompt user for last name as well)
 	return 0;
 }
